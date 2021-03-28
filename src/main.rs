39a911674924f897813bb2606b81d53f2e68c1fd
bloatoo <bloatoo@tuta.io>
2021-03-28 14:01:30 +0300

    Dynamic showing of word definitions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+#![feature(async_closure)]
 mod event;
 
 use event::*;
-use std::io;
+use serde_json::Value;
+use std::{io, sync::mpsc, thread};
 use std::cmp::Ordering;
 
 use tui::{Terminal, backend::TermionBackend, layout::{Alignment, Constraint, Direction, Layout, Margin}, style::{Color, Modifier, Style}, text::{Span, Spans, Text}, widgets::Paragraph};
 use termion::{event::Key, raw::IntoRawMode};
 use std::time::Instant;
 use std::env;
+
+// use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
@@ -20,21 +24,39 @@ async fn main() -> Result<(), io::Error> {
     if let Err(_) = word_count {
         word_count = Ok(10)
     }
-    let events = Events::new();
+
+    let mut definition_string = String::new();
+
     let client = reqwest::Client::new();
+
+    let res = client.get(&format!("https://random-word-api.herokuapp.com/word?number={}", word_count.unwrap())[..]);
+    let text = &res.send().await.unwrap().text().await.unwrap()[..];
+
+    let words: Vec<&str> = serde_json::from_str(&text[..]).unwrap();
+
+    let word_string = words.join(" ");
+
+    for word in words {
+        let other_res = client.get(&format!("https://api.dictionaryapi.dev/api/v2/entries/en_US/{}", word)[..]).send().await.unwrap().text().await.unwrap();
+
+        let json: Value = serde_json::from_str(&other_res[..]).unwrap();
+        let value = json[0]["meanings"][0]["definitions"][0]["definition"].as_str();
+
+        if let Some(val) = value {
+            definition_string.push_str(&(String::from(val) + "\n")[..]);
+        } else {
+            definition_string.push_str("No definitions found\n");
+        }
+    }
+
+    let events = Events::new();
 
     let mut current_index: usize = 1;
     let mut input_string: String = String::new();
     let mut end = false;
     let mut time_taken: u128 = 0;
-
-    let res = client.get(&format!("https://random-word-api.herokuapp.com/word?number={}", word_count.unwrap())[..]).send().await.unwrap();
-
-    let text = res.text().await.unwrap();
-    let words: Vec<&str> = serde_json::from_str(&text[..]).unwrap();
-
-
-    let word_string = words.join(" ");
+    let mut timer: Instant = Instant::now();
+    let mut timer_is_going = false;
 
     let chunks = Layout::default()
         .direction(Direction::Vertical)
@@ -44,15 +66,13 @@ async fn main() -> Result<(), io::Error> {
 
     print!("{}", termion::clear::All);
     print!("{}", termion::cursor::SteadyBar);
-
-    let now = Instant::now();
    
     loop {
         terminal.draw(|f| {
             let mut to_be_rendered_str: Vec<Span> = vec![];
 
             if end {
-                let wpm = (word_string.len() as f64 / 5 as f64) / ((time_taken as f64 / 1000 as f64) / 60 as f64);
+                let wpm = (word_string.len() as f64 / 5f64) / ((time_taken as f64 / 1000f64) / 60f64);
 
                 let blue = Style::default().fg(Color::Blue);
 
@@ -87,10 +107,13 @@ async fn main() -> Result<(), io::Error> {
                     }
                 }
 
-                let wpm = (input_string.len() as f64 / 5 as f64) / ((now.elapsed().as_millis() as f64 / 1000 as f64) / 60 as f64);
+                let wpm = (input_string.len() as f64 / 5 as f64) / ((timer.elapsed().as_millis() as f64 / 1000 as f64) / 60 as f64);
 
-                f.render_widget(Paragraph::new(Text::from(format!("WPM: {:.2}", wpm))).alignment(Alignment::Center), chunks[0].inner(&Margin { horizontal: 0, vertical: chunks[0].height / 3 }));
+                let defs: Vec<&str> = definition_string.split("\n").collect();
+
+                f.render_widget(Paragraph::new(Text::from(format!("WPM: {:.2}", wpm))).alignment(Alignment::Center), chunks[0].inner(&Margin { horizontal: 0, vertical: chunks[0].height / 4 }));
                 f.render_widget(Paragraph::new(Spans::from(to_be_rendered_str.clone())).alignment(Alignment::Center), chunks[0].inner(&Margin { horizontal: 0, vertical: chunks[0].height / 2 }));
+                f.render_widget(Paragraph::new(Spans::from(defs[input_string.split(" ").collect::<Vec<&str>>().len() - 1].clone())).alignment(Alignment::Center), chunks[0].inner(&Margin { horizontal: 0, vertical: chunks[0].height / 3 }));
 
                 f.set_cursor(chunks[0].x + chunks[0].width / 2 + current_index as u16 - to_be_rendered_str.len() as u16 / 2, chunks[0].y + chunks[0].height / 2);
             }
@@ -100,13 +123,17 @@ async fn main() -> Result<(), io::Error> {
             match event {
                 Key::Char(c) => {
                     if !end {
+                        if !timer_is_going {
+                            timer_is_going = true;
+                            timer = Instant::now();
+                        }
                         if current_index <= word_string.len() {
                             current_index += 1;
                             input_string.push(c);
                         }
                         if word_string == input_string {
                             end = true;
-                            time_taken = now.elapsed().as_millis();
+                            time_taken = timer.elapsed().as_millis();
                         }
                     } else {
                         match c {
