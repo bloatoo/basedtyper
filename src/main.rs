0dc056431594ecef8998e08272de415dffe67b9d
bloatoo <bloatoo@tuta.io>
2021-04-03 18:45:04 +0300

    Add quote gamemode (unstable)

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,31 @@ use app::{App, State};
 
 use std::{cmp::Ordering, env, io};
 
+use rand::Rng;
+
 use event::*;
 use word::Word;
 
 use serde_json::Value;
 use termion::{event::Key, raw::IntoRawMode};
-use tui::{Terminal, backend::TermionBackend, layout::{Alignment, Constraint, Corner, Direction, Layout, Margin}, style::{Color, Modifier, Style}, text::{Span, Spans, Text}, widgets::Paragraph};
+use tui::{Terminal, backend::TermionBackend, layout::{Alignment, Constraint, Direction, Layout, Margin}, style::{Color, Modifier, Style}, text::{Span, Spans, Text}, widgets::Paragraph};
 
 fn usage(args: &[String]) {
     println!(
         "basedtyper
 
         \rusage:\n \
-        \r {} random <word count>            fetches random words and their definitions from APIs
-        \r {} wordlist <path to wordlist>    uses a local file as a wordlist
+        \r {arg} random <word count>            fetches random words and their definitions from APIs
+        \r {arg} wordlist <path to wordlist>    uses a local file as a wordlist
+        \r {arg} quote                          fetches a random post from r/copypasta (UNSTABLE)
         
         \roptions:\n \
         \r --no-defs                       disable definitions for words
         ",
-        &args[0], &args[0]
-    )
+        arg = &args[0]
+    );
+
+    std::process::exit(0);
 }
 
 #[tokio::main]
@@ -35,22 +40,19 @@ async fn main() -> Result<(), io::Error> {
 
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
-        usage(&args);
-        std::process::exit(1);
-    }
-
     let mut words: Vec<Word> = Vec::new();
+
+    let client = reqwest::Client::new();
 
     match &args[1][..] {
         "random" => {
+            if args.len() < 3 { usage(&args); }
             let mut word_count = env::args().collect::<Vec<String>>()[2].parse::<u32>();
 
             if word_count.is_err() {
                 word_count = Ok(10)
             }
 
-            let client = reqwest::Client::new();
             let res = client.get(
                 &format!(
                     "https://random-word-api.herokuapp.com/word?number={}",
@@ -69,7 +71,7 @@ async fn main() -> Result<(), io::Error> {
                             &format!(
                                 "https://api.dictionaryapi.dev/api/v2/entries/en_US/{}",
                                 word
-                            )[..],
+                            )[..]
                         )
                         .send()
                         .await
@@ -94,7 +96,26 @@ async fn main() -> Result<(), io::Error> {
             }
         }
 
+        "quote" => {
+            let other_res = client.get("https://www.reddit.com/r/copypasta/top/.json?sort=top&t=week&showmedia=false&mediaonly=false&is_self=true&limit=100")
+                .send()
+                .await
+                .unwrap()
+                .text()
+                .await
+                .unwrap();
+
+            let json: Value = serde_json::from_str(&other_res[..]).unwrap();
+
+            let quote = json["data"]["children"][rand::thread_rng().gen_range(0..100) as usize]["data"]["selftext"].as_str().unwrap();
+            
+            for word in quote.split(" ") {
+                words.push(Word::new(word, ""));
+            }
+        }
+
         "wordlist" => {
+            if args.len() < 3 { usage(&args); }
             let parsed_words = wordlist_parser::parse(&args[2], &args).await;
 
             if let Err(err) = parsed_words {
@@ -127,153 +148,163 @@ async fn main() -> Result<(), io::Error> {
 
     let word_string = words_vec.join(" ");
 
-    let word_string = word_string.trim_end();
+    let mut word_string = word_string.trim_end();
 
     let events = Events::new();
 
     let words_split = word_string.split("").collect::<Vec<&str>>();
 
+
     let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(100)])
+        .direction(Direction::Vertical)
         .margin(3)
+        .constraints([Constraint::Percentage(100)])
         .split(terminal.size().unwrap());
+
+    if word_string.len() as u16 > chunks[0].width {
+        word_string = &word_string[..rand::thread_rng().gen_range(11..chunks[0].width - 2) as usize];
+    }
+
+    let re = regex::Regex::new("[^\x00-\x7F]+").unwrap();
+
+    let word_string = re.replace_all(word_string, "");
+    let word_string = word_string.trim_end();
+
 
     print!("{}", termion::clear::All);
     print!("{}", termion::cursor::SteadyBar);
 
     loop {
-        terminal
-            .draw(|f| {
-                let mut to_be_rendered_str: Vec<Span> = vec![];
-                
-                match app.state {
-                    State::MainMenu => {
-                        let mut spans: Vec<Spans> = vec![];
+        terminal.draw(|f| {
+            let mut to_be_rendered_str: Vec<Span> = vec![];
+            
+            match app.state {
+                State::MainMenu => {
+                    let mut spans: Vec<Spans> = vec![];
 
-                        for _ in 0..chunks[0].height / 2 - 2 {
-                            spans.push(Spans::default());
-                        }
-
-                        spans.append(&mut vec![
-                            Spans::from(Span::styled("basedtyper", Style::default().fg(Color::Green))),
-                            Spans::from(Span::raw("")),
-                            Spans::from(Span::raw("t to start typing game")),
-                        ]);
-
-                        f.render_widget(Paragraph::new(spans).alignment(Alignment::Center), chunks[0]);
+                    for _ in 0..chunks[0].height / 2 - 2 {
+                        spans.push(Spans::default());
                     }
 
-                    State::EndScreen => {
-                        let wpm = (word_string.len() as f64 / 5_f64)
-                            / ((app.time_taken as f64 / 1000_f64) / 60_f64);
-    
-                        let blue = Style::default().fg(Color::Blue);
-    
-                        let mut spans: Vec<Spans> = vec![];
+                    spans.append(&mut vec![
+                        Spans::from(Span::styled("basedtyper", Style::default().fg(Color::Green))),
+                        Spans::from(Span::raw("")),
+                        Spans::from(Span::raw("t to start typing game")),
+                    ]);
 
-                        for _ in 0..chunks[0].height / 2 - 3 {
-                            spans.push(Spans::default());
-                        }
+                    f.render_widget(Paragraph::new(spans).alignment(Alignment::Center), chunks[0]);
+                }
 
-                        spans.append(&mut vec![
-                            Spans::from(vec![Span::styled("WPM", blue.add_modifier(Modifier::BOLD)),
-                                Span::styled(format!(": {:.2}", wpm), blue)]),
+                State::EndScreen => {
+                    let wpm = (word_string.len() as f64 / 5_f64)
+                        / ((app.time_taken as f64 / 1000_f64) / 60_f64);
+   
+                    let blue = Style::default().fg(Color::Blue);
+ 
+                    let mut spans: Vec<Spans> = vec![];
 
-                            Spans::from(vec![Span::styled("Time used", blue.add_modifier(Modifier::BOLD)),
-                                Span::styled(format!(": {:.1}s", app.time_taken as f64 / 1000_f64), blue)]),
-
-                            Spans::from(vec![Span::raw("")]),
-
-                            Spans::from(vec![Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-                                Span::raw(" to quit")]),
-
-                            Spans::from(vec![Span::styled("r", Style::default().add_modifier(Modifier::BOLD)),
-                                Span::raw(" to restart")]),
-
-                            Spans::from(vec![Span::styled("m", Style::default().add_modifier(Modifier::BOLD)),
-                                Span::raw(" to go to the main menu")])
-                        ]);
-
-                        f.render_widget(
-                            Paragraph::new(spans).alignment(Alignment::Center),
-                            chunks[0]
-                        );
+                    for _ in 0..chunks[0].height / 2 - 3 {
+                        spans.push(Spans::default());
                     }
 
-                    State::TypingGame => {
-                        for (index, c) in word_string.split("").enumerate() {
-                            if app.input_string.split("").nth(index).is_some() {
-                                match index.cmp(&app.current_index) {
-                                    Ordering::Less => {
-                                        if app.input_string.split("").collect::<Vec<&str>>()[index] != words_split[index] {
-                                            to_be_rendered_str.push(Span::styled(c, Style::default().fg(Color::Red)));
-    
-                                        } else {
-                                            to_be_rendered_str.push(Span::styled(
-                                                c,
-                                                Style::default().fg(Color::DarkGray),
-                                            ));
-                                        }
+                    spans.append(&mut vec![
+                        Spans::from(vec![Span::styled("WPM", blue.add_modifier(Modifier::BOLD)),
+                            Span::styled(format!(": {:.2}", wpm), blue)]),
+
+                        Spans::from(vec![Span::styled("Time used", blue.add_modifier(Modifier::BOLD)),
+                            Span::styled(format!(": {:.1}s", app.time_taken as f64 / 1000_f64), blue)]),
+
+                        Spans::from(vec![Span::raw("")]),
+
+                        Spans::from(vec![Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
+                            Span::raw(" to quit")]),
+
+                        Spans::from(vec![Span::styled("r", Style::default().add_modifier(Modifier::BOLD)),
+                            Span::raw(" to restart")]),
+
+                        Spans::from(vec![Span::styled("m", Style::default().add_modifier(Modifier::BOLD)),
+                            Span::raw(" to go to the main menu")])
+                    ]);
+
+                    f.render_widget(
+                        Paragraph::new(spans).alignment(Alignment::Center),
+                        chunks[0]
+                    );
+                }
+
+                State::TypingGame => {
+                    for (index, c) in word_string.split("").enumerate() {
+                        if app.input_string.split("").nth(index).is_some() {
+                            match index.cmp(&app.current_index) {
+                                Ordering::Less => {
+                                    if app.input_string.split("").collect::<Vec<&str>>()[index] != words_split[index] {
+                                        to_be_rendered_str.push(Span::styled(c, Style::default().fg(Color::Red)));
+
+                                    } else {
+                                        to_be_rendered_str.push(Span::styled(
+                                            c,
+                                            Style::default().fg(Color::DarkGray),
+                                        ));
                                     }
-    
-                                    _ => to_be_rendered_str.push(Span::styled(c, Style::default().fg(Color::White))),
                                 }
 
-                            } else {
-                                to_be_rendered_str.push(Span::styled(c, Style::default()));
+                                _ => to_be_rendered_str.push(Span::styled(c, Style::default().fg(Color::White))),
                             }
+
+                        } else {
+                            to_be_rendered_str.push(Span::styled(c, Style::default()));
                         }
-
-                        let wpm = (app.input_string.len() as f64 / 5_f64)
-                            / (if app.timer.is_some() { (app.timer.unwrap().elapsed().as_millis() as f64 / 1000_f64) / 60_f64 } else { 0_f64 });
-    
-                        let defs: Vec<&String> =
-                            words.iter().map(|elem| elem.get_definition()).collect();
-    
-                        f.render_widget(
-                            Paragraph::new(Text::from(format!("WPM: {:.2}", wpm)))
-                                .alignment(Alignment::Center),
-                            chunks[0].inner(&Margin {
-                                horizontal: 0,
-                                vertical: chunks[0].height / 4,
-                            }),
-                        );
-    
-                        f.render_widget(
-                            Paragraph::new(Spans::from(to_be_rendered_str.clone()))
-                                .alignment(Alignment::Center),
-                            chunks[0].inner(&Margin {
-                                horizontal: 0,
-                                vertical: chunks[0].height / 2,
-                            }),
-                        );
-    
-                        let index = app.input_string.split(' ').count() - 1;
-
-                        f.render_widget(
-                            Paragraph::new(Spans::from(if defs.len() > index {
-                                defs[index].clone()
-                            } else {
-                                String::new()
-                            }))
-                            .alignment(Alignment::Center),
-                            chunks[0].inner(&Margin {
-                                horizontal: 0,
-                                vertical: chunks[0].height / 3,
-                            }),
-                        );
-    
-                        f.set_cursor(
-                            chunks[0].x + chunks[0].width / 2 + app.current_index as u16
-                                - to_be_rendered_str.len() as u16 / 2,
-                            chunks[0].y + chunks[0].height / 2,
-                        );
                     }
-                _ => (),
+
+                    let wpm = (app.input_string.len() as f64 / 5_f64)
+                        / (if app.timer.is_some() { (app.timer.unwrap().elapsed().as_millis() as f64 / 1000_f64) / 60_f64 } else { 0_f64 });
+
+                    let defs: Vec<&String> =
+                        words.iter().map(|elem| elem.get_definition()).collect();
+
+                    f.render_widget(
+                        Paragraph::new(Text::from(format!("WPM: {:.2}", wpm)))
+                            .alignment(Alignment::Center),
+                        chunks[0].inner(&Margin {
+                            horizontal: 0,
+                            vertical: chunks[0].height / 4,
+                        }),
+                    );
+
+                    f.render_widget(
+                        Paragraph::new(Spans::from(to_be_rendered_str.clone()))
+                            .alignment(Alignment::Center),
+                        chunks[0].inner(&Margin {
+                            horizontal: 0,
+                            vertical: chunks[0].height / 2,
+                        }),
+                    );
+
+                    let index = app.input_string.split(' ').count() - 1;
+
+                    f.render_widget(
+                        Paragraph::new(Spans::from(if defs.len() > index {
+                            defs[index].clone()
+                        } else {
+                            String::new()
+                        }))
+                        .alignment(Alignment::Center),
+                        chunks[0].inner(&Margin {
+                            horizontal: 0,
+                            vertical: chunks[0].height / 3,
+                        }),
+                    );
+
+                    f.set_cursor(
+                        chunks[0].x + chunks[0].width / 2 + app.current_index as u16
+                            - to_be_rendered_str.len() as u16 / 2,
+                        chunks[0].y + chunks[0].height / 2,
+                    );
                 }
-            })
-            .unwrap();
+            _ => (),
+            }
+        })
+        .unwrap();
 
         if let Ok(Event::Input(event)) = events.next() {
             match event {
@@ -315,7 +346,7 @@ async fn main() -> Result<(), io::Error> {
                                 app.input_string.push(c);
                             }
 
-                            if word_string == app.input_string {
+                            if word_string.trim() == app.input_string {
                                 app.state = State::EndScreen;
                                 app.time_taken = if app.timer.is_some() { app.timer.unwrap().elapsed().as_millis() } else { 0 };
                             }
