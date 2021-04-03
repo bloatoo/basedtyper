354638276b977bee1bb969ce93f21a48f3b4edc3
bloatoo <bloatoo@tuta.io>
2021-04-03 19:19:20 +0300

    Fix program crashing when no arguments are provided, disable LTO

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,10 @@ async fn main() -> Result<(), io::Error> {
 
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        usage(&args);
+    }
+
     let mut words: Vec<Word> = Vec::new();
 
     let client = reqwest::Client::new();
